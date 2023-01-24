0e46b947d7cbf52c0b2495b6f502e6e39c5348ff
Ryusei1115 <tekunikaru22@gmail.com>
2023-01-24 12:07:24 +0900

    WIP

---
+++ 20230124/app.js
@@ -0,0 +1,46 @@
+Vue.createApp({
+    data(){
+        return{
+            lastName: "",
+            firstName: "",
+            date: new Date(),
+            nickName: "",
+            error: "",
+            lists: ["課題1を終わらせる", "課題2を終わらせる", "課題3を終わらせる"],
+            taskName: "",
+        };
+    },
+    methods:{
+        addTask(){
+            this.lists.push(this.taskName);
+        }
+    },
+    // 計算型プロパティ
+    computed:{
+        fullName(){
+            return `${this.lastName} ${this.firstName}`;
+        },
+        nickNameData(){
+            return `${this.nickName}`;
+        },
+        //日付の加工
+        dataFormat(){
+            const dataArray = ["日", "月", "火", "水", "木", "金", "土"];
+
+            return `${this.date.getFullYear()}年${this.date.getMonth()+1}月${this.date.getDate()}日${dataArray[this.date.getDay()]}曜日`;
+        },
+    },
+    // 変更を感知し処理を行うオプション
+    watch:{
+        lastName(newName, oldName){
+            this.error = newName.length > 5 ? "5文字以内でお願いします" : "";
+        },
+        lists:{
+            handler(newTask, oldtask){
+                // sessionStrageにデータを一時保存する
+                sessionStorage.setItem(newTask, newTask);
+            },
+            deep: true,
+        }
+    }
+}).mount("#app");
