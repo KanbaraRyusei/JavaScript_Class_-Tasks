3285fde28653226ea16f1225ee39fb9bda2325a0
Ryusei1115 <tekunikaru22@gmail.com>
2023-01-31 12:20:38 +0900

    WIP

--- 20230131/app.js
+++ 20230131/app.js
@@ -4,8 +4,40 @@ const App = Vue.createApp({
         taskName: "",
         dateData: "",
         priorities: ["低", "中", "高"],
+        priority: "",
+        done: false,
         lists: [],
     };
     },
+    methods:{
+        addTask(){
+            if(this.taskName == "" || this.dateData == "" || this.priority == ""){
+                return;
+            }
+            var tasks = {
+                id: Date.now(),
+                done: this.done,
+                taskName: this.taskName,
+                date: this.dateData,
+                priority: this.priority,
+                publish: new Date(),
+            };
+            this.lists.push(tasks);
+            this.resetTask();
+        },
+        resetTask(){
+            this.taskName = "";
+            this.dateData = "";
+            this.priority = 1;
+        }
+    },
+    watch:{
+        lists:{
+            handler(newTask, oldtask){
+                sessionStorage.setItem("newTasks", JSON.stringify(newTask));
+            },
+            deep: true,
+        }
+    }
 });
 App.mount("#app");
